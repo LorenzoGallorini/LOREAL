cdb9f31b6842d9359d871045abcbb23a6fe4c3b2
Lorenzo Gallorini <l.gallorini1@campus.unimib.it>
2020-05-05 18:14:05 +0200

    BOMBAAA!!!

--- WORKSPACE/app/src/main/java/com/example/cinemhub/ui/home/HomeFragment.java
+++ WORKSPACE/app/src/main/java/com/example/cinemhub/ui/home/HomeFragment.java
@@ -11,16 +11,18 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.cinemhub.R;
+import com.example.cinemhub.ui.notifications.NotificationsFragment;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private final String TAG = "HomeFragment";
-
+    //private OnFragmentInteractionListener mListener;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
@@ -30,8 +32,12 @@ public class HomeFragment extends Fragment {
         Show1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "onClick: d");
-
+                Log.d(TAG, "onClick: Button1");
+                Fragment newFragment = new NotificationsFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.nav_host_fragment, newFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
             }
         });
         return root;
