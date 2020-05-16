f370210940090be9fbc05376523b4dc7ef44ef56
Lorenzo Gallorini <l.gallorini1@campus.unimib.it>
2020-05-16 23:35:39 +0200

    API_KEY Google

--- WORKSPACE/app/src/main/java/com/example/cinemhub/utils/Constants.java
+++ WORKSPACE/app/src/main/java/com/example/cinemhub/utils/Constants.java
@@ -3,7 +3,7 @@ package com.example.cinemhub.utils;
 public class Constants {
     public static final String FAVOURITE_SHARED_PREF_FILE_NAME = "SharedFavouriteFile";
     public static final String FAVOURITE_SHARED_PREF_NAME = "Favourite";
-
+    public static final String API_KEY_YOUTUBE = "AIzaSyDOXC5SWaXM8uYH3DPOD2NWt3CQd3y85mc";
 
 
 }
